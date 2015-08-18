621d4ebbdd6858cc459aab932ea35066e716606f
vka <vadimsks@inbox.lv>
2015-08-18 22:56:00 +0300

    autocommit

--- src/utils.js
+++ src/utils.js
@@ -467,8 +467,9 @@ str_do_smth = function( cr, where, what )
 								cr.pos.getRangeTo(o.pos)<10; } });
 
 	    if(!target) {
-		targets = cr.pos.findClosestByRange(FIND_MY_STRUCTURES,
+		target = cr.pos.findClosestByRange(FIND_MY_STRUCTURES,
 						    {filter: function(o) { return o.structureType==STRUCTURE_STORAGE } } );
+
 	    }
 	    if(target) {
 		cr.moveTo(target);
