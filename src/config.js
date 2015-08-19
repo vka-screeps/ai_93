e845bcbefe439340f129e2dec649c8ecba6e8692
vka <vadimsks@inbox.lv>
2015-08-19 22:56:37 +0300

    autocommit

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     
     updateConfig() {
-	var newVersion = 4;
+	var newVersion = 5;
 	if(Memory.configVer != newVersion) {
 	    Memory.configVer = newVersion;
 	    console.log('Apply Config - ' + newVersion);
