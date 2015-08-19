c01b805faafca2dfbe3042907038e61dfe5b1434
vka <vadimsks@inbox.lv>
2015-08-19 23:10:15 +0300

    autocommit

--- src/myroom.js
+++ src/myroom.js
@@ -14,16 +14,16 @@ module.exports = function () {
 	// energy dropped
 	var res_flag = room.find(FIND_FLAGS, { filter: function(o) { return o.name.substring(0,3) == 'res'; } } )[0];
 	var res_pos=null;
-	if(res_flag)
-	    res_pos = res_flag.pos;
-	else
-	    console.log( 'flag res not found ');
-	    
-	var el = room.find(FIND_DROPPED_ENERGY, { filter: function(o) { return o.energy>50 && ( !res_pos || res_pos.getRangeTo(o.pos)<10 ) } });
 	var esum = 0;
-	for(var i in el) {
-	    var e = el[i];
-	    esum = esum + e.energy
+	
+	if(res_flag) {
+	    res_pos = res_flag.pos;
+	    
+	    var el = room.find(FIND_DROPPED_ENERGY, { filter: function(o) { return o.energy>50 && ( !res_pos || res_pos.getRangeTo(o.pos)<10 ) } });
+	    for(var i in el) {
+		var e = el[i];
+		esum = esum + e.energy
+	    }
 	}
 
 	var el = room.find(FIND_MY_STRUCTURES,
