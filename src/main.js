758d95e1cbd755a9b48e63d6043c2196d4b1e5e2
vka <vadimsks@inbox.lv>
2016-03-14 23:45:26 +0200

    autocommit

--- src/main.js
+++ src/main.js
@@ -5,7 +5,140 @@ var myroom = require('myroom');
 var u = require('utils');
 var config = require('config');
 var stat = require('stat');
-var r = require('roles');
+//var r = require('roles');
+
+r = {
+    init: function() { m_init() },
+    f : f,
+    cf : F,
+    planSpawnJobs : planSpawnJobs,
+}
+
+
+// Factory
+var F = class {
+    constructor() {
+	this.tbl={}
+    }
+
+    reg(c) {
+	u.log("Registering: " + c.cname(), u.LOG_INFO);
+	this.tbl[c.cname()] = c;
+    }
+
+    make(d, parent) {
+	let cls = this.tbl[d.cname];
+	if ( cls  ) {
+	    u.log("Instantiating: " + d.cname, u.LOG_INFO); 
+	    return new cls(d, parent);
+	} else {
+	    u.log("Can't find class: " + d.cname, u.LOG_WARN);
+	}
+    }
+};
+
+var f = new F();
+
+function m_init() {
+    regClasses(allClasses);
+}
+
+
+
+function regClasses( list ) {
+    if(!f)
+	f = new F();
+    
+    list.forEach( function(c) {
+	f.reg(c); } );
+}
+
+
+class CMemObj {
+    constructor(d, parent) {
+	this.d = d;
+	this.parent = parent;
+    }
+
+    getObjLogName() {
+	return this.d.cname + "(" + this.d.name + ", " + this.d.id + ")";
+    }
+
+    getObj() {
+	if( this.d && this.d.id )
+	    return Game.getObjectById(this.d.id);
+
+	u.log( "Can't find object - " + this.getObjLogName(), u.LOG_WARN);
+	return null;
+    }
+}
+
+class Job extends CMemObj {
+    constructor(d, parent) {
+	super(d, parent);
+    }
+
+    static cname() { return 'Job'; }
+}
+
+class JobMiner extends Job {
+    constructor(d, parent) {
+	super(d, parent);
+    }
+
+    static cname() { return 'JobMiner'; }
+}
+
+class JobCarrier extends Job {
+    constructor(d, parent) {
+	super(d, parent);
+    }
+
+    static cname() { return 'JobCarrier'; }
+}
+
+class JobSpawn extends Job {
+    constructor(d, parent) {
+	super(d, parent);
+    }
+
+    static cname() { return 'JobSpawn'; }
+}
+
+// class CCreep extends CMemObj {
+//     constructor(d, parent) {
+// 	super(d, parent);
+// 	this.croom = this.parent.parent.rooms[d.id_room];
+// 	this.role = f.make(d.role, this);
+//     }
+// }
+
+var allClasses = [ Job, JobMiner, JobCarrier, JobSpawn ];
+
+
+///////////////////////////////////////////////////////
+function planSpawnJobs(rm) {
+    let jobs = rm.memory.jobs;
+    if (!jobs['JobSpawn']) jobs['JobSpawn'] = {};
+    let lst = jobs['JobSpawn'];
+    
+    for(let i in rm.memory.balance) {
+	let bal_ln = rm.memory.balance[i];
+	if(bal_ln.count > bal_ln.curCount) {
+	    let job_id = bal_ln.id;
+	    if(!lst[new_job]) {
+		let new_job = {
+		    id: job_id,
+		    design: bal_ln.design;
+		};
+
+		u.log("New JobSpawn: " + job_id, u.LOG_INFO); 		
+		lst.push(new_job);
+	    }
+	}
+    }
+}
+
 
 //config.rooms = [];
 
