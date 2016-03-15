b8eea3e59b30c5c49f37269df6c16d3e4e297971
vka <vadimsks@inbox.lv>
2016-03-16 01:22:07 +0200

    autocommit

--- src/main.js
+++ src/main.js
@@ -757,52 +757,6 @@ module.exports = {
 	// planGoals();
 
 	// runGoals();
-
-	for(var name in Game.creeps) {
-	    var creep = Game.creeps[name];
-
-	    if(creep.memory.role == 'harvester') {
-		u.crstr_do_harvest(creep);
-		// harvester(creep);
-	    }
-
-	    else if(creep.memory.role == 'workonly') {
-		u.crstr_do_workonly(creep);
-		// harvester(creep);
-	    }    
-
-	    else if(creep.memory.role == 'harv') {
-		u.crstr_do_harv(creep);
-	    }
-
-	    else if(creep.memory.role == 'carry') {
-		u.crstr_do_carry(creep);
-	    }
-
-	    else if(creep.memory.role == 'archer') {
-		u.crstr_do_archer(creep);
-	    }    
-
-	    if(creep.memory.role == 'cr') {
-		u.crstr_do_control(creep);
-	    }
-
-	    if(creep.memory.role == 'builder') {
-		u.crstr_do_build(creep);
-	    }
-
-	    if(creep.memory.role == 'testtgt') {
-		u.crstr_do_testtgt(creep);
-	    }    
-	    
-	    if(creep.memory.role == 'guard') {
-		var targets = creep.room.find(FIND_HOSTILE_CREEPS);
-		if(targets.length) {
-		    creep.moveTo(targets[0]);
-		    creep.attack(targets[0]);
-		}
-	    }
-	}
     },
 
     printAll : function() {
