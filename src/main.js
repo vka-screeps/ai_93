3cc3a68007c78eea6769980871fe49ac2bf87bd9
vka <vadimsks@inbox.lv>
2016-03-16 01:45:13 +0200

    autocommit

--- src/main.js
+++ src/main.js
@@ -89,15 +89,15 @@ class Job extends CMemObj {
 
 	let d = this.d;
 	if(d.taken_by_id) {
-	    /*
-	    u.log( "Job.unassign id - " + d.id, u.LOG_INFO);
+
+	    // u.log( "Job.unassign id - " + d.id, u.LOG_INFO);
 	    let cr = Game.getObjectById(d.taken_by_id);
 
-	    if(cr) {
-		let role = cr.memory.role;
-		role.job_id = null;
-		role.workStatus = null;
-	    } */
+	    //if(cr) {
+	    let role = cr.memory.role;
+	    role.job_id = null;
+	    role.workStatus = null;
+	// }
 	    /*else {
 		// find the creep's memory
 		u.log( "Performance warning - Can't find creep with id - " + d.taken_by_id, u.LOG_WARN);
