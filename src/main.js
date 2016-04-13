cf324ea7d2cc6f7e2f67e1497e58ae7dc7615bf5
vka <vadimsks@inbox.lv>
2016-04-14 00:48:53 +0300

    autocommit

--- src/main.js
+++ src/main.js
@@ -475,7 +475,37 @@ class AddrHarvPoint extends Addr {
 	let d = this.d;
 	if(d.res_id) {
 	    let res = Game.getObjectById(d.res_id);
-	    let srez = PathFinder.search( cr.pos, { pos: res.pos, range: 1 } );
+	    let srez = PathFinder.search( cr.pos, { pos: res.pos, range: 1 }, {
+		// We need to set the defaults costs higher so that we
+		// can set the road cost lower in `roomCallback`
+		plainCost: 2,
+		swampCost: 10,
+		
+		roomCallback: function(roomName) {
+		    let room = Game.rooms[roomName];
+		    // In this example `room` will always exist, but since PathFinder 
+		    // supports searches which span multiple rooms you should be careful!
+		    if (!room) return;
+		    let costs = new PathFinder.CostMatrix;
+		    room.find(FIND_STRUCTURES).forEach(function(structure) {
+			if (structure.structureType === STRUCTURE_ROAD) {
+			    // Favor roads over plain tiles
+			    costs.set(structure.pos.x, structure.pos.y, 1);
+			} else if (structure.structureType !== STRUCTURE_RAMPART || 
+				   !structure.my) {
+			    // Can't walk through buildings, except for our own ramparts
+			    costs.set(structure.pos.x, structure.pos.y, 0xff);
+			}
+		    });
+		    // Avoid creeps in the room
+		    /*
+		    room.find(FIND_CREEPS).forEach(function(creep) {
+			costs.set(creep.pos.x, creep.pos.y, 0xff);
+		    });
+		    */
+		    return costs;
+		},
+	    } );
 	    if(srez.path.length > dist) {
 		let p2 = srez.path[0];
 		cr.move(cr.pos.getDirectionTo(p2));
