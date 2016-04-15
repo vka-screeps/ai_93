2c827ffddcef1392b3545d1e69285772e9f5dafc
vka <vadimsks@inbox.lv>
2016-04-16 00:58:16 +0300

    autocommit

--- src/main.js
+++ src/main.js
@@ -1205,23 +1205,22 @@ class JobCarrier extends Job {
 	    let tt = f.make(d.take_to);
 	    if(tt.d.isSpawn) {
 		if(cr.carry[RESOURCE_ENERGY] === 0) {
+		    if(role.workStatus.trip_start_time) {
+			let trip_time = Game.time - role.workStatus.trip_start_time+1;
+			if(!d.avg_trip_time) {
+			    d.avg_trip_time = trip_time;
+			} else {
+			    d.avg_trip_time = 0.7 * d.avg_trip_time + 0.3 * trip_time;
+			}
+			role.workStatus.trip_start_time = 0;
+		    }		    
 		    role.workStatus.step = 0;
 		} else {
 		    let tgt = this.findTarget(rm, cr);
 		    if(tgt) {
 			if( cr.transfer(tgt, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE ) {
 			    cr.moveTo(tgt);
-			} else {
-			    if(role.workStatus.trip_start_time) {
-				let trip_time = Game.time - role.workStatus.trip_start_time+1;
-				if(!d.avg_trip_time) {
-				    d.avg_trip_time = trip_time;
-				} else {
-				    d.avg_trip_time = 0.7 * d.avg_trip_time + 0.3 * trip_time;
-				}
-				role.workStatus.trip_start_time = 0;
-			    }
-			}
+			} 
 		    } else {
 			if(role.workStatus.trip_start_time)
 			    role.workStatus.trip_start_time++; // don't count this as trip time
