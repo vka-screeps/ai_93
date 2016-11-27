6ad86e5400297cb3ccf26ae8929fe60c644afca7
vka <vadimsks@inbox.lv>
2016-11-27 19:47:57 +0200

    Fixed path finding for AddrHarvPoint

--- src/main.js
+++ src/main.js
@@ -1166,82 +1166,66 @@ class AddrHarvPoint extends Addr {
 	}
 	dist = defaultFor(dist, 3);
 
+	let dest_pos;
 	if(d.res_id) {
 	    let res = Game.getObjectById(d.res_id);
-	    let srez = PathFinder.search( cr.pos, { pos: res.pos, range: 1 }, {
-		// We need to set the defaults costs higher so that we
-		// can set the road cost lower in `roomCallback`
-		plainCost: 2,
-		swampCost: 10,
-		maxOps: 4000,
-		roomCallback: function(roomName) {
-		    let room = Game.rooms[roomName];
-		    // In this example `room` will always exist, but since PathFinder 
-		    // supports searches which span multiple rooms you should be careful!
-		    if (!room) return;
-		    let costs = new PathFinder.CostMatrix;
-		    room.find(FIND_STRUCTURES).forEach(function(structure) {
-			if (structure.structureType === STRUCTURE_ROAD) {
-			    // Favor roads over plain tiles
-			    costs.set(structure.pos.x, structure.pos.y, 1);
-			} else if (structure.structureType !== STRUCTURE_RAMPART || 
-				   !structure.my) {
-			    // Can't walk through buildings, except for our own ramparts
-			    costs.set(structure.pos.x, structure.pos.y, 0xff);
-			}
-		    });
-		    // Avoid creeps in the room
-
-
-		    room.find(FIND_CREEPS).forEach(function(creep) {
-			costs.set(creep.pos.x, creep.pos.y, 30);
-		    });
-
-
-		    return costs;
-		},
-	    } );
-
-	    /*
-	    console.log('searchRez for ' + cr.name + ' to ' + res.pos + ' - ' + srez.path.length+ ', ' + srez.ops );
-	    {
-		let str = '';
-		for(let i=0; i<srez.path.length; ++i) {
-		    str = str + srez.path[i];
-		    if(i==3)
-			break;
-		}
-		console.log(str);		
-	    }
-	    */
-
-	    
-	    if(srez.path.length > dist) {
-		let p2 = srez.path[0];
-		cr.move(cr.pos.getDirectionTo(p2));
-		return true;
-	    }
-	    /*
-	    if(cr.pos.getRangeTo(res) > dist) {
-		//let searchRez = ;
-		// console.log('searchRez = ' + searchRez.path.length);
-		// u.printObject(searchRez);
-
-		u.log("Moving to object " + res, u.LOG_DBG);
-		cr.moveTo(res);
-		return true;
-	    } else {
-	    let srez = PathFinder.search( cr.pos, { pos: res.pos, range: 1 } );
-		if(srez.path.length > 4) {
-		    
-		}
-	    }
-	    */
+	    dest_pos = { pos: res.pos, range: 1 };
 	} else {
-	    if(cr.pos.getRangeTo(d.x, d.y) > dist) {
-		cr.moveTo(d.x, d.y);
-		return true;
-	    }
+	    dest_pos = { pos: this.getPos(), range: 1 };
+	}
+	
+	let srez = PathFinder.search( cr.pos, dest_pos, {
+	    // We need to set the defaults costs higher so that we
+	    // can set the road cost lower in `roomCallback`
+	    plainCost: 2,
+	    swampCost: 10,
+	    maxOps: 4000,
+	    roomCallback: function(roomName) {
+		let room = Game.rooms[roomName];
+		// In this example `room` will always exist, but since PathFinder 
+		// supports searches which span multiple rooms you should be careful!
+		if (!room) return;
+		let costs = new PathFinder.CostMatrix;
+		room.find(FIND_STRUCTURES).forEach(function(structure) {
+		    if (structure.structureType === STRUCTURE_ROAD) {
+			// Favor roads over plain tiles
+			costs.set(structure.pos.x, structure.pos.y, 1);
+		    } else if (structure.structureType !== STRUCTURE_RAMPART || 
+			       !structure.my) {
+			// Can't walk through buildings, except for our own ramparts
+			costs.set(structure.pos.x, structure.pos.y, 0xff);
+		    }
+		});
+		// Avoid creeps in the room
+
+
+		room.find(FIND_CREEPS).forEach(function(creep) {
+		    costs.set(creep.pos.x, creep.pos.y, 30);
+		});
+
+
+		return costs;
+	    },
+	} );
+
+	/*
+	  console.log('searchRez for ' + cr.name + ' to ' + res.pos + ' - ' + srez.path.length+ ', ' + srez.ops );
+	  {
+	  let str = '';
+	  for(let i=0; i<srez.path.length; ++i) {
+	  str = str + srez.path[i];
+	  if(i==3)
+	  break;
+	  }
+	  console.log(str);		
+	  }
+	*/
+
+	
+	if(srez.path.length > dist) {
+	    let p2 = srez.path[0];
+	    cr.move(cr.pos.getDirectionTo(p2));
+	    return true;
 	}
 	return false;
     }
